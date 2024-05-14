98a61c48646bff646710346378b0f1a372da3d17
Chase Dymont <87335271+Chamont0127@users.noreply.github.com>
2024-05-13 18:38:28 -0700

    Player movement script implementation
    player movement script now moves cuber in game

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,23 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     #region Variables
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private float speed;
 
     [SerializeField] private InputManager inputManager;
+
+    private Vector3 moveVector = new Vector3();
+
     #endregion
 
     #region Functions
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
     private void Start()
     {
         inputManager.OnPlayerMovementInputVectorChange += InputManager_OnPlayerMovementInputVectorChange;
     }
 
+    private void FixedUpdate()
+    {
+        characterController.Move(moveVector * speed * Time.deltaTime);
+    }
+
     private void InputManager_OnPlayerMovementInputVectorChange(object sender, InputManager.OnPlayerMovementInputVectorChangeEventArgs e)
     {
-        Debug.Log(e.inputVectorNormalized);
+        moveVector.x = e.inputVectorNormalized.x;
+        moveVector.z = e.inputVectorNormalized.y;
     }
     #endregion
 }
